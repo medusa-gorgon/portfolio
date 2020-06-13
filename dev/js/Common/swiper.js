dda3101d452cb7147b5c314ad0c75fe055f64480
Vlad Gerasimovich <vlad.gerasimovich.dev@gmail.com>
2020-06-13 14:20:12 +0300

    Hero section Code Review

--- dev/js/Common/swiper.js
+++ dev/js/Common/swiper.js
@@ -1,10 +1,10 @@
 import Swiper from 'swiper';
 
 const heroSwiper = new Swiper('.swiper-container', {
-    loop: true,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.hero__swiper__button__next',
-        prevEl: '.hero__swiper__button__prev',
-    },
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.hero__swiper-button_next',
+    prevEl: '.hero__swiper-button_prev',
+  },
 });
