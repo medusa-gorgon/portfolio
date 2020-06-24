af6e72b4e53cf92c7a4b1fdbab7b91fc58cfe6e1
Masha Shevchuk <mg.bizarre@gmail.com>
2020-06-24 12:26:46 +0300

    Add FontAwesome and change px to rem

--- gulp/configs/config.js
+++ gulp/configs/config.js
@@ -3,7 +3,7 @@ module.exports = {
   dev: './dev',
   vendorCSS: [
     './node_modules/normalize.css/normalize.css',
-    './node_modules/swiper/css/swiper.min.css'
+    './node_modules/swiper/css/swiper.css'
   ],
   appJS: './dev/js/**/*.js',
 };
